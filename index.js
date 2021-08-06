0eed6dffde0f3cf9e1eeb685026fe45a6537debc
mahi1212 <mahi121.mr@gmail.com>
2021-08-06 15:11:17 +0600

    not completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-let lastVideo = document.querySelector
+let listVideo = document.querySelector('.video-list .vid');
+let mainVideo = document.querySelector('.main-video video');
+let title = document.querySelector('.main-video .title');
+
+listVideo.forEach(video =>{
+    video.onclick() = () =>{
+        listVideo.forEach(vid => vid.classList.remove('active'));
+        video.classList.add('active');
+        if(video.classList.contains('active')){
+            let src = video.children[0].getAttribute('src');
+            mainVideo.src = src;
+            let text = video.children[1].innerHTML;
+            title.innerHTML = text;
+        }
+    }
+});
